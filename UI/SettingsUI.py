240951790a6efb18cf6b89c7598c208e5aca3817
MasKrs <695758673@qq.com>
2025-04-26 16:32:18 +0800

    增加免责声明

--- UI/SettingsUI.py
+++ UI/SettingsUI.py
@@ -269,6 +269,6 @@ class Ui_SettingDialog(object):
         self.lb_doc.setText(_translate("SettingDialog", "说明文档："))
         self.label_2.setText(_translate("SettingDialog", "软件版本："))
         self.lb_version.setText(_translate("SettingDialog", "     v2.8.3"))
-        self.label.setText(_translate("SettingDialog", "<html><head/><body><p><span style=\" font-size:10pt;\">Copyright </span><span style=\" font-size:16pt;\">© </span><span style=\" font-size:10pt;\">2024 WKhistory&amp;MasKrs, Inc. All rights reserved.</span></p><p><span style=\" font-size:7pt;\">Icons by </span><a href=\"https://icons8.com\"><span style=\" font-size:7pt; text-decoration: underline; color:#0000ff;\">Icons8</span></a></p></body></html>"))
+        self.label.setText(_translate("SettingDialog", "<html><head/><body><p align=\"center\"><span style=\" font-size:6pt; font-weight:600;\">免责声明</span></p><p align=\"center\"><span style=\" font-size:7pt;\">本工具仅限学习交流使用，应在获取软件的24小时内删除。挂机行为存在被ban风险，使用本工具，风险自负，任何后果与本工具无关。</span></p><p><span style=\" font-size:7pt;\">Icons by </span><a href=\"https://icons8.com\"><span style=\" font-size:7pt; text-decoration: underline; color:#0000ff;\">Icons8</span></a></p></body></html>"))
         self.tabWidget.setTabText(self.tabWidget.indexOf(self.tab_2), _translate("SettingDialog", "关于"))
 import pictures_rc
